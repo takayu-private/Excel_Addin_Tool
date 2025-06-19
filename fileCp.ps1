3cf9e2eb359636a88d4ed02f420982feaf52d02d
takayu-private <62892066+takayu-private@users.noreply.github.com>
2025-06-19 09:48:24 +0900

    アドイン追加に伴う修正
    関数拡張アドインもコピー対象に追加

--- fileCp.ps1
+++ fileCp.ps1
@@ -6,27 +6,31 @@ $SearchAddinFile = "SearchExtensionAddin.xlam"
 $FunctionAddinFile = "FunctionExtensionAddin.xlam"
 $addinFileList = $SearchAddinFile, $FunctionAddinFile
 
-# 元のアドインファイルのパス
-$sourceAddinPath = "$currentPath\SearchExtensionAddin.xlam"
-
 # ユーザ名を取得
 $userProfile = [Environment]::GetEnvironmentVariable("USERPROFILE")
 
 # コピー先のパス
 $destinationPath = "$userProfile\AppData\Roaming\Microsoft\AddIns\"
 
-# ファイルをコピー
-Copy-Item -Path $sourceAddinPath -Destination $destinationPath -Force
+foreach ($filePath in $addinFileList) {
 
-if (-not (Test-Path $sourceAddinPath)) {
-  New-Item -Type File "$currentPath\copy_ng"
+  # アドインファイルのパス
+  $addinPath = "$currentPath\$filePath"
+
+  # ファイルをコピー
+  Copy-Item -Path $addinPath -Destination $destinationPath -Force
+
+  if (-not (Test-Path $addinPath)) {
+    New-Item -Type File "$currentPath\copy_ng"
+  }
 }
 
 # ショートカットを作成
 $WshShell = New-Object -ComObject WScript.Shell
-$ShortcutPath = ".\AddIns.lnk"
+$ShortcutPath = "$currentPath\AddIns.lnk"
 $Shortcut = $WshShell.CreateShortcut($ShortcutPath)
 $Shortcut.TargetPath = $destinationPath
 $Shortcut.Save()
+
 # COM オブジェクトの解放
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($WshShell) | Out-Null
